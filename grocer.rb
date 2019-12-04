5e2ca9ba5043c62a05d13ab73e805826432edce5
Zeshan Raja <zraja4402@gmail.com>
2019-12-04 04:34:13 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -31,8 +31,8 @@ counter = 0
 
  while counter < cart.length do   
    new_cart_item = find_item_by_name_in_collection(cart[counter][:item], new_cart)
-   if new_cart_item != nil
-    new_item[:count] += 1 
+   if new_cart_item_item != nil
+    new_cart_item[:count] += 1 
    else 
      new_item = {
       :item => cart[counter][:item],
