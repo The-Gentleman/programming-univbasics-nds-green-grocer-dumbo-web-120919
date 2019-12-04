9581beb6c5e0f7b96ca2d2509d0040d3926b9e56
Zeshan Raja <zraja4402@gmail.com>
2019-12-04 04:54:47 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -49,9 +49,12 @@ new_cart
 end
 
 def apply_coupons(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+
+
+
+
+
+
 end
 
 def apply_clearance(cart)
