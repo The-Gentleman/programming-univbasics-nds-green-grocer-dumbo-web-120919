2bfd3860d211c312a9b215e1feb2293ff05e65b6
Zeshan Raja <zraja4402@gmail.com>
2019-12-04 04:31:54 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -31,7 +31,7 @@ counter = 0
 
  while counter < cart.length do   
    new_item = find_item_by_name_in_collection(cart[counter][:item], array)
-   if new_item 
+   if new_item != nil
     new_item[:count] += 1 
    else 
      new_item = {
